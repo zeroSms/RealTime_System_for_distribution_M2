c187a64560fec325bd49fdaf7278dd548138d5e8
zeroSms <peruna4535@gmail.com>
2023-07-24 13:28:00 +0900

    2023/07/24

--- head_nod_analysis/setup_variable.py
+++ head_nod_analysis/setup_variable.py
@@ -11,7 +11,8 @@ path = os.getcwd()
 # server_address = '192.168.2.111'
 # server_address = '192.168.2.22'  # 3A, notePC
 # server_address = '172.19.0.44'  # cs-wlan, notePC
-server_address = '192.168.0.2'  # 4H-aterm, notePC
+# server_address = '192.168.0.2'  # 4H-aterm, notePC
+server_address = '192.168.0.18'  # 自宅, notePC
 
 port_num = {'1': {'audience': 63307}}
 
